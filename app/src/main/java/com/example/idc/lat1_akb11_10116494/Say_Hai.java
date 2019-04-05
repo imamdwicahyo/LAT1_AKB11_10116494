1803fbcdc895b52c579bacdb6db7b03b493be05b
Unknown <imamdwicahyo29@gmail.com>
2019-04-05 19:45:06 +0700

    Layout_boiodata (100%)

---
+++ app/src/main/java/com/example/idc/lat1_akb11_10116494/Say_Hai.java
@@ -0,0 +1,13 @@
+package com.example.idc.lat1_akb11_10116494;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Say_Hai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_say__hai);
+    }
+}
